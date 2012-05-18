9b102cee9305e268d9d30fe5e66817e51e637c25
Alex Griessman <alex.griessman@gmail.com>
2012-05-18 18:38:49 -0400

    Replaced the functions with ones computed from Maxima

--- func1a.m
+++ func1a.m
@@ -3,9 +3,11 @@ function pi = func1a(xk, xkprev, blah)
 l = 1;
 k = 1;
 
+A = xk(1);
+B = xk(2);
 %P = xk(3);
-alpha = xk(3);
+a = xk(3);
 P = blah;
 
-pi = 1/6*k*l^3*cos(xk(1))*(8*sin(xk(1))+3*sin(xk(2))) - P*l*(alpha*cos(xk(1))+sin(xk(2)));
+pi = -l*sin(A)*P-a*l*cos(A)*P+(k*(3*l^3*cos(A)*sin(B)+6*l^3*cos(A)*sin(A)))/6+(k*l^3*cos(A)*sin(A))/3;
 pi = pi - arc(xk, xkprev, blah);
--- func2a.m
+++ func2a.m
@@ -3,10 +3,12 @@ function pi = func2a(xk, xkprev, blah)
 l = 1;
 k = 1;
 
+A = xk(1);
+B = xk(2);
 %P = xk(3);
-alpha = xk(3);
+a = xk(3);
 P = blah;
 
-pi = 1/6*k*l^3*cos(xk(2))*(3*sin(xk(1))+2*sin(xk(2)))-P*l*(alpha*cos(xk(2))+sin(xk(2)));
+pi = -l*sin(B)*P-a*l*cos(B)*P+(k*(2*l^3*cos(B)*sin(B)+3*l^3*sin(A)*cos(B)))/6;
 
 pi = pi - arc(xk, xkprev, blah);
